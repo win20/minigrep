79c3fa4a8af10383d6d813bb1f36c0647c6bab9e
win20 <win.barua2@gmail.com>
2023-05-30 23:43:51 +0100

    added test

--- target/debug/minigrep.d
+++ target/debug/minigrep.d
@@ -1,1 +1,1 @@
-/home/winb/Dev/_repos/minigrep/target/debug/minigrep: /home/winb/Dev/_repos/minigrep/src/main.rs
+/home/winb/Dev/_repos/minigrep/target/debug/minigrep: /home/winb/Dev/_repos/minigrep/src/lib.rs /home/winb/Dev/_repos/minigrep/src/main.rs
